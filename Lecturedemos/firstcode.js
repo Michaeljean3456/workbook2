e73dc19f7d9a79c24832baf11cfab00d25c3fce6
Michael JeanBaptiste <michaeljean3456@gmail.com>
2023-04-24 17:06:22 -0400

    day 1

---
+++ Lecturedemos/firstcode.js
@@ -0,0 +1,20 @@
+"use strict;"
+
+var firstname;
+
+firstname= "Mike"
+
+console.log("Your name is " + firstname);
+
+var word, num;
+num = "banana";
+word = num + 13;
+
+console.log(word + num)
+
+var custid
+var firstmidlastname
+var customergender;
+var custDOB;
+var driverlicensenum;
+var autopolicynum;
